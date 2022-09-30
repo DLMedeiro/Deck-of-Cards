3d7d186bbd78fad44e3bc88c567fa5a282434e71
DanielleM <dlmedeiro@gmail.com>
2022-09-30 15:57:19 -0700

    auto draw functioning

--- src/DrawCards.js
+++ src/DrawCards.js
@@ -10,8 +10,9 @@ const DrawCards = () => {
   const [num, setNum] = useState(0);
   const [pause, setPause] = useState(true);
   const [cardDeck, setCardDeck] = useState([]);
-
+  const [time, setTime] = useState(false);
   //   get deck of cards from API
+
   useEffect(() => {
     axios
       .get("http://deckofcardsapi.com/api/deck/new/draw/?count=52")
@@ -20,18 +21,43 @@ const DrawCards = () => {
           res.data.cards.map(({ value, suit }) => [`${value} of ${suit}`])
         );
       });
-  }, [draw]);
+  }, [ready]);
 
-  useEffect(() => {
+  function shuffle() {
+    setReady(true);
+  }
+
+  // function pullCard(num) {
+  //   if (num < 52) {
+  //     // setDrawnCards(cardDeck[num]);
+  //     num += 1;
+  //     console.log(num);
+  //   } else {
+  //     clearInterval(timerId.current);
+  //   }
+  // }
+
+  function drawingCards() {
+    setDraw(true);
     timerId.current = setInterval(() => {
-      if (pause === false && num <= 52) {
-        setNum((num) => num + 1);
-      }
-    }, 1000);
+      setNum((num) => num + 1);
+    }, 100);
+
     return () => {
       clearInterval(timerId.current);
     };
-  }, [ready, pause]);
+  }
+
+  // useEffect(() => {
+  //   timerId.current = setInterval(() => {
+  //     if (pause === true && num <= 52) {
+  //       setNum((num) => num + 1);
+  //     }
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(timerId.current);
+  //   };
+  // }, [ready, pause]);
 
   useEffect(() => {
     if (pause === false && num >= 0) {
@@ -50,36 +76,37 @@ const DrawCards = () => {
   }
 
   function restart() {
-    setPause(true);
-    setDraw(true);
-    setDrawnCards([]);
-    setCardDeck([]);
-    setReady(true);
+    clearInterval(timerId.current);
     setNum(0);
+    setReady(false);
+    setDraw(false);
+
+    setPause(true);
+    // setDrawnCards([]);
+    setCardDeck([]);
   }
 
   return (
     <div>
       {ready ? (
-        <div>
-          <h3>{cardDeck[num]}</h3>
-          <h3>{num}</h3>
-          {pause ? (
-            <button onClick={startStop}>Draw</button>
-          ) : (
-            <button onClick={startStop}>Pause</button>
-          )}
-
+        draw ? (
           <div>
-            {drawnCards.map((c) => (
-              <Card card={c} />
-            ))}
+            <button onClick={restart}>Shuffle Deck</button>
+            {num < 52 ? (
+              <div>{cardDeck[num]}</div>
+            ) : (
+              <div>No More Cards Available</div>
+            )}
           </div>
-        </div>
+        ) : (
+          <div>
+            <button onClick={drawingCards}>Draw Cards</button>
+          </div>
+        )
       ) : (
         <div>
-          <h3>All cards drawn</h3>
-          <button onClick={restart}>Shuffle</button>
+          <h3>Shuffle Deck</h3>
+          <button onClick={shuffle}>Shuffle</button>
         </div>
       )}
     </div>
